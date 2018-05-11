ebb44599231d40c2ac8db06495efd141bb0534d3
Macky Beltran <macky@kearnstechnology.com>
2018-05-11 00:21:00 -0400

    Fix server.js to be react router ready for deployment

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const express = require('express')
 module.exports = {
   app: function () {
     const app = express()
-    const indexPath = path.join(__dirname, 'index.html')
     const publicPath = express.static(path.join(__dirname, 'public'))
 
     app.use('/public', publicPath)
-    app.get('/', function (_, res) { res.sendFile(indexPath) })
+    app.get('/*', function(req, res) {
+      res.sendFile(path.join(__dirname, '/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+    }
+  })
+})
 
     return app
   }
